54c5e4bc82b9116a2d31b62a8676f9b07cf164dd
Charlie Richardson <Charlie.Richardson@Nology.local>
2020-02-27 11:02:29 +0000

    styling

--- scripts.js
+++ scripts.js
@@ -31,30 +31,34 @@ class MotherShip extends AttackShip {
     constructor(name,type,hitPoints,damage){
         super(name,type,hitPoints,damage);
     }
-    destroyAll() {
-        
-    }
+
 }
 
 const shipArray = [];
+let shipArrayLength = shipArray.length;
 const attackShipBuilder = [0,1,2,3,4];
 const defenceShipBuilder = [0,1,2,3,4,5,6,7];
 
 attackShipBuilder.forEach( number => shipArray.push(new AttackShip(("A" + number),"attack",45, 12)));
 defenceShipBuilder.forEach( number => shipArray.push(new Defenceship(("D" + number),"defence",80, 10)));
 shipArray.push(new MotherShip("M","mothership",100, 9))
-
 shipArray.forEach(ship => ship.displayShip());
 
-const shootShip = (shipType) => shipType.minusHitpoints();
-    
+const shootShip = (shipType) => shipType.minusHitpoints();    
 const checkAlive = (shipType) => shipType.isALive();
-
 const randomNumber = () => Math.floor(Math.random() * shipArray.length);
 
 const fireButton = () => {
     const damageShip = randomNumber();
-    shootAlien(shipArray[damageShip]);
+    shootShip(shipArray[damageShip]);
     checkAlive(shipArray[damageShip]);
+    checkMotherShip();
 }
 
+const destoryAll = () => shipArray.forEach(ship => { 
+    ship.hitPoints = 0; 
+    ship.isALive();})
+
+const checkMotherShip = () => shipArray[shipArrayLength].hitPoints <= 0 ? destoryAll() : null;
+
+checkMotherShip();
