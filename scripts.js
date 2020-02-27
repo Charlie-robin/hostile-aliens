9e929c95a3a8d776e8aff8fe105fb63427c557c2
Charlie Richardson <Charlie.Richardson@Nology.local>
2020-02-27 12:39:17 +0000

    js updated

--- scripts.js
+++ scripts.js
@@ -20,47 +20,48 @@ class AttackShip {
         this.hitPoints <= 0 ? document.getElementById(this.name).style.display = "none" : null;
     }
 }
-
 class Defenceship extends AttackShip {
     constructor(name,type,hitPoints,damage){
         super(name,type,hitPoints,damage);
     }
 }
-
 class MotherShip extends AttackShip {
     constructor(name,type,hitPoints,damage){
         super(name,type,hitPoints,damage);
     }
 }
-
-const shipArray = [];
-let shipArrayLength = shipArray.length;
+let shipArray = [];
 const attackShipBuilder = [0,1,2,3,4];
 const defenceShipBuilder = [0,1,2,3,4,5,6,7];
-
-const newGame = () => {
-attackShipBuilder.forEach( number => shipArray.push(new AttackShip(("A" + number),"attack",45, 12)));
-defenceShipBuilder.forEach( number => shipArray.push(new Defenceship(("D" + number),"defence",80, 10)));
-shipArray.push(new MotherShip("M","mothership",100, 9))
-shipArray.forEach(ship => ship.displayShip());
-document.querySelector("main").insertAdjacentHTML("beforeend", '<button onclick="fireButton()">Fire</button>')
-document.querySelector("header").classList.toggle("hide");
+let shipArrayLength;
+const clearGame = () => { 
+    document.getElementById("mothership").innerHTML = "";
+    document.getElementById("attack").innerHTML = "";
+    document.getElementById("defence").innerHTML = "";
 }
-
+const newGame = () => {
+    clearGame();
+    shipArray = [];
+    attackShipBuilder.forEach( number => shipArray.push(new AttackShip(("A" + number),"attack",45, 12)));
+    defenceShipBuilder.forEach( number => shipArray.push(new Defenceship(("D" + number),"defence",80, 10)));
+    shipArray.push(new MotherShip("M","mothership",100, 9));
+    shipArrayLength = shipArray.length;
+    shipArray.forEach(ship => ship.displayShip());
+    document.getElementById("button").classList.toggle("hide");
+    document.querySelector("header").classList.toggle("hide");
+}
 const shootShip = (shipType) => shipType.minusHitpoints();    
 const checkAlive = (shipType) => shipType.isALive();
 const randomNumber = () => Math.floor(Math.random() * shipArray.length);
-
+const destoryAll = () => {
+    shipArray.forEach(ship => {ship.hitPoints = 0; ship.isALive();});
+    document.getElementById("button").classList.toggle("hide");
+    document.querySelector("header").classList.toggle("hide");
+}
+const checkMotherShip = () => shipArray[shipArrayLength-1].hitPoints <= 0 ? destoryAll() : null;
 const fireButton = () => {
     const damageShip = randomNumber();
     shootShip(shipArray[damageShip]);
     checkAlive(shipArray[damageShip]);
     checkMotherShip();
 }
-
-const destoryAll = () => shipArray.forEach(ship => { 
-    ship.hitPoints = 0; 
-    ship.isALive();})
-
-const checkMotherShip = () => shipArray[shipArrayLength].hitPoints <= 0 ? destoryAll() : null;
-
