36b65f7d1d2b72ba58a7492f0bf105448f9f86b0
Charlie Richardson <Charlie.Richardson@Nology.local>
2020-02-27 10:11:21 +0000

    js Working

---
+++ scripts.js
@@ -0,0 +1,60 @@
+class AttackShip {
+    constructor(name,type,hitPoints,damage){
+        this.name = name;
+        this.type = type;
+        this.hitPoints = hitPoints;
+        this.damage = damage;
+        this.isAlive = true;
+    }
+    minusHitpoints() {
+        this.hitPoints -= this.damage;
+        this.updateShip()
+    }
+    updateShip() {
+        document.getElementById(this.name).innerHTML = `<p id='${this.name}' >${this.name} : ${this.hitPoints}</p>`;
+    }
+    displayShip() {
+        document.getElementById(this.type).innerHTML += `<p id='${this.name}' >${this.name} : ${this.hitPoints}</p>`;
+    }
+    isALive() {
+        this.hitPoints <= 0 ? document.getElementById(this.name).style.display = "none" : null;
+    }
+}
+
+class Defenceship extends AttackShip {
+    constructor(name,type,hitPoints,damage){
+        super(name,type,hitPoints,damage);
+    }
+}
+
+class MotherShip extends AttackShip {
+    constructor(name,type,hitPoints,damage){
+        super(name,type,hitPoints,damage);
+    }
+    destroyAll() {
+        
+    }
+}
+
+const shipArray = [];
+const attackShipBuilder = [0,1,2,3,4];
+const defenceShipBuilder = [0,1,2,3,4,5,6,7];
+
+attackShipBuilder.forEach( number => shipArray.push(new AttackShip(("A" + number),"attack",45, 12)));
+defenceShipBuilder.forEach( number => shipArray.push(new Defenceship(("D" + number),"defence",80, 10)));
+shipArray.push(new MotherShip("M","mothership",100, 9))
+
+shipArray.forEach(ship => ship.displayShip());
+
+const shootShip = (shipType) => shipType.minusHitpoints();
+    
+const checkAlive = (shipType) => shipType.isALive();
+
+const randomNumber = () => Math.floor(Math.random() * shipArray.length);
+
+const fireButton = () => {
+    const damageShip = randomNumber();
+    shootAlien(shipArray[damageShip]);
+    checkAlive(shipArray[damageShip]);
+}
+
